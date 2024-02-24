3ca430ed6814810c28826324c6ffa24c70b4b032
AryanAD <aryanhohai@gmail.com>
2024-02-24 21:26:50 +0545

    Added gitignore

--- VolcanoEruptionCW.java
+++ VolcanoEruptionCW.java
@@ -1,9 +1,10 @@
-// A volcano periodically blasts. The details are:
-// YEAR | SMOKE_GENERATED | LAVA_FLOW | LAVA_HEIGHT
-// 1560 | 35km | 2 | 500m
-// 1620 | 10km | 1 | 200m
-// 1710 | 25km | 1.5 | 235m
-// 1835 | 15km | 1.2 | 198m
-// 1920 | 10km | 1.1 | 558m
-
-// - Calculate Average of all and when the next occurance might happen.
+/*
+A volcano periodically blasts. The details are:
+ YEAR | SMOKE_GENERATED | LAVA_FLOW | LAVA_HEIGHT
+ 1560 | 35km | 2 | 500m
+ 1620 | 10km | 1 | 200m
+ 1710 | 25km | 1.5 | 235m
+ 1835 | 15km | 1.2 | 198m
+ 1920 | 10km | 1.1 | 558m
+ - Calculate Average of all and when the next occurance might happen.
+ */
