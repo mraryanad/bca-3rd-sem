4e1fc441e5aced19f316897267b3c307ca3682e5
AryanAD <aryanhohai@gmail.com>
2024-05-18 08:23:07 +0545

    php

--- dsa-C/postfixToInfix.c
+++ dsa-C/postfixToInfix.c
@@ -1,5 +1,65 @@
-#include<stdio.h>
-int main() {
-    
-return 0;
+// postfix to infix DSA
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 100
+
+char stack[MAX_SIZE];
+int top = -1;
+
+void push(char c)
+{
+    stack[++top] = c;
+}
+
+char pop()
+{
+    return stack[top--];
+}
+
+int isOperator(char c)
+{
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+}
+
+void convertPostfixToInfix(char postfix[])
+{
+    char infix[MAX_SIZE] = "";
+    int i, j;
+
+    for (i = 0; postfix[i] != '\0'; i++)
+    {
+        char c = postfix[i];
+
+        if (isOperator(c))
+        {
+            char op1 = pop();
+            char op2 = pop();
+            char temp[MAX_SIZE];
+            sprintf(temp, "(%c%c%c)", op2, c, op1);
+            strcat(infix, temp);
+            push(temp[0]);
+        }
+        else
+        {
+            char temp[2] = {c, '\0'};
+            push(c);
+            strcat(infix, temp);
+        }
+    }
+
+    printf("Infix expression: %s\n", infix);
+}
+
+int main()
+{
+    char postfix[MAX_SIZE];
+    printf("Enter a postfix expression: ");
+    scanf("%s", postfix);
+
+    convertPostfixToInfix(postfix);
+
+    return 0;
 }
